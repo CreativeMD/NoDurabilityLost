fdd2815641ede4b4488081a20d18c7c4fc715516
CreativeMD <Creative-Multimedia-Design@gmx.de>
2024-12-17 14:39:32 +0100

    Implemented functionality Fabric

---
+++ src/fabric/java/team/creative/nodurabilitylost/NoDurabilityLost.java
@@ -0,0 +1,10 @@
+package team.creative.nodurabilitylost;
+
+import net.fabricmc.api.ModInitializer;
+
+public class NoDurabilityLost implements ModInitializer {
+    
+    @Override
+    public void onInitialize() {}
+    
+}
